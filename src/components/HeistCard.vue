12a33e9966a3019dca615c3a305ac1ff42b72030
Deathstructor <paulchast@gmail.com>
2024-01-29 13:41:35 +0100

    Moved all database files to a different repo. Added tab icon

---
+++ src/components/HeistCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>{{ GetName() }}</h2>
+        <h4>difficulty</h4>
+        <h4>tactic</h4>
+    </div>
+</template>
+
+<script setup>
+async function GetName() {
+    return (await HeistModel
+        .find({})
+        .exec())
+        .map(e => {
+            return {
+                name: e.name,
+                warehouseId: e.warehouseId,
+                job: e.job,
+                schedule: e.schedule,
+                assignedOrder: e.assignedOrder
+            }
+        })
+}
+
+</script>
