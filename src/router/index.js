32024c5b897fcaf74292cd83703b37687279a560
Paul Chastain <paulchast1@gmail.com>
2024-01-29 21:03:07 +0100

    Added electron to the project to make it a webapplication

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import HeistView from '../views/HeistView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: '/',
