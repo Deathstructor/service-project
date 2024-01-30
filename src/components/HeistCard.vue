70190e19531cbc4e6a29d1a5c74a8e4b00ccebbc
Paul Chastain <paulchast1@gmail.com>
2024-01-30 17:03:16 +0100

    Made MongoDB work with Vue and Electron. Also made the program fetch data from the db and display it on the website

--- src/components/HeistCard.vue
+++ src/components/HeistCard.vue
@@ -1,25 +1,43 @@
-<template>
-    <div>
-        <h2>{{ GetName() }}</h2>
-        <h4>difficulty</h4>
-        <h4>tactic</h4>
-    </div>
-</template>
-
 <script setup>
+import { MongoClient } from 'mongodb';
+import { ref, toRaw } from 'vue'
+
+const uri = "mongodb+srv://chastainpaul:uqja15ElJY5hRZSf@heistnetdb.gtpzx.mongodb.net/HeistnetDB?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri)
+const database = client.db("HeistnetDB")
+const collection = database.collection("heists")
+
 async function GetName() {
-    return (await HeistModel
-        .find({})
-        .exec())
-        .map(e => {
-            return {
-                name: e.name,
-                warehouseId: e.warehouseId,
-                job: e.job,
-                schedule: e.schedule,
-                assignedOrder: e.assignedOrder
-            }
-        })
+    const matchCursor = await collection.find()
+
+    const ret = [];
+    for await (let heist of matchCursor) {
+        ret.push(heist)
+    }
+
+    return ret;
 }
 
+const heists = ref(null);
+const returnName = async () => heists.value = toRaw(await GetName())
+returnName()
+
 </script>
+
+<template>
+    <div v-if="heists">
+        <div v-for="item in heists" class="h-card">
+            <h2>{{ item.name }}</h2>
+            <h4>Difficulty: {{ item.difficulty }}</h4>
+            <h4>Tactic: {{ item.tactic }}</h4>
+            <div v-for="job in item.job">
+                <h4>Title: {{ job.job_title }}</h4>
+                <h4>Available: {{ job.available }}</h4>
+            </div>
+        </div>
+    </div>
+    <div v-else>
+        <h1>Loading...</h1>
+    </div>
+</template>
--- src/views/HeistView.vue
+++ src/views/HeistView.vue
@@ -1,5 +1,8 @@
 <template>
+    <h1>Heists</h1>
+    <HeistCard />
 </template>
 
 <script setup>
+import HeistCard from '@/components/HeistCard.vue';
 </script>
